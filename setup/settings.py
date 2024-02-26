754d01fa2ed2936a88cb5e7573ab5211e5298961
GustavoMarquesMartins <gustavomarquesmartins11@gmail.com>
2024-02-26 11:53:39 -0300

    define tempo de vida de um token JWT

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+# Tempo de vida do token de acesso (access token)
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
+
